f8d7e0b6bb6931a5526639b4aa123281b9512432
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-10 15:01:51 +0100

    committing adding balance changes

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+
+  it 'responds to balance' do
+    expect(subject).to respond_to :balance
+  end
+
   it 'expect oystercard to have default starting value of "0"' do
     expect(subject.balance).to eq(0)
   end
